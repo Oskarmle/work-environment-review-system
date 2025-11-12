ed23e9f99f410996695b135c3538fae478653a7d
OskarEriksen <omle.eriksen@gmail.com>
2025-11-12 15:54:14 +0100

    Feat: Create data-source file and install TypeORM

---
+++ backend/src/data-source.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenv.config();
+
+export const dbConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+const datasource = new DataSource(dbConfig as DataSourceOptions);
+export default datasource;
