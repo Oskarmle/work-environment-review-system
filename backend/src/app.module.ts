b8304991b74ee6e19a4784ac37c30d0f3d5839ae
OskarEriksen <omle.eriksen@gmail.com>
2025-11-12 16:03:00 +0100

    feat: Add TypeORM migration commands and configure database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { dbConfig } from './database/data-source';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forRoot(dbConfig)],
+})
 export class AppModule {}
--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -11,7 +11,16 @@ export const dbConfig: TypeOrmModuleOptions = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  synchronize: false,
+  entities: [],
+  migrations: ['dist/database/migrations/*{.ts,.js}'],
 };
 
-const datasource = new DataSource(dbConfig as DataSourceOptions);
+const dataSourceOptions: DataSourceOptions = {
+  ...dbConfig,
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/database/migrations/*{.ts,.js}'],
+} as DataSourceOptions;
+
+const datasource = new DataSource(dataSourceOptions);
 export default datasource;
