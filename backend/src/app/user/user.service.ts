5331a96b287c29f60cef5b0d43f8067111f8ba9d
OskarEriksen <omle.eriksen@gmail.com>
2025-11-18 13:09:00 +0100

    feat: create user method in user service

--- backend/src/app/user/user.service.ts
+++ backend/src/app/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -12,5 +13,10 @@ export class UsersService {
 
   findOne(email: string): Promise<User | null> {
     return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(user);
   }
 }
