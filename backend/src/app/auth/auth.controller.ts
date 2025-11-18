b7426716e3b11bc1d361074ffa27d031782aaf26
OskarEriksen <omle.eriksen@gmail.com>
2025-11-18 13:40:03 +0100

    feat: implement signup and login methods in AuthController and AuthService

--- backend/src/app/auth/auth.controller.ts
+++ backend/src/app/auth/auth.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { Request as ExpressRequest } from 'express';
 import { LocalAuthGuard } from 'src/shared/guards/local-auth.guard';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { AuthService } from './auth.service';
+import { LoginUserDto } from '../user/dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  @UseGuards(LocalAuthGuard)
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.signup(createUserDto);
+  }
+
   @Post('login')
-  login(@Request() req: ExpressRequest) {
-    return req.user;
+  async login(@Body() loginUserDto: LoginUserDto) {
+    return await this.authService.login(loginUserDto);
   }
 
   @UseGuards(LocalAuthGuard)
--- backend/src/app/auth/auth.service.ts
+++ backend/src/app/auth/auth.service.ts
@@ -1,9 +1,14 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from '../user/user.service';
 import { User } from '../user/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import * as brycpt from 'bcrypt';
+import { LoginUserDto } from '../user/dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -12,26 +17,58 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(
-    email: string,
-    hashedPassword: string,
-  ): Promise<User | null> {
-    const user = await this.usersService.findOne(email);
-    if (user && user.hashedPassword === hashedPassword) {
+  async validateUser(email: string, password: string): Promise<User | null> {
+    const user = await this.usersService.findOne({
+      where: { email: email },
+      select: {
+        id: true,
+        email: true,
+        hashedPassword: true,
+      },
+    });
+    if (!user) {
+      return null;
+    }
+
+    const isMatch = await brycpt.compare(password, user.hashedPassword);
+    if (isMatch) {
       return user;
     }
     return null;
   }
 
-  async login(user: User) {
-    const payload = { email: user.email, sub: user.id };
+  async login(loginUserDto: LoginUserDto) {
+    const user = await this.usersService.findOne({
+      where: { email: loginUserDto.email },
+      select: {
+        id: true,
+        email: true,
+        hashedPassword: true,
+      },
+    });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const isMatch = await brycpt.compare(
+      loginUserDto.password,
+      user.hashedPassword,
+    );
+
+    if (!isMatch) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { sub: user.id, email: user.email };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
   }
 
   async signup(createUserDto: CreateUserDto) {
-    const user = await this.usersService.findOne(createUserDto.email);
+    const user = await this.usersService.findOne({
+      where: { email: createUserDto.email },
+    });
     if (user) {
       throw new BadRequestException('User with this email already exists');
     }
