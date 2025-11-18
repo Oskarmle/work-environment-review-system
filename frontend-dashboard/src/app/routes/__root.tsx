23640aec52513f149edc249ff88e4a7f83c73972
OskarEriksen <omle.eriksen@gmail.com>
2025-11-18 16:49:56 +0100

    feat: cleanup files

--- frontend-dashboard/src/app/routes/__root.tsx
+++ frontend-dashboard/src/app/routes/__root.tsx
@@ -2,19 +2,19 @@ import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 const RootLayout = () => (
-  <>
-    <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{' '}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
-    </div>
-    <hr />
+  <div
+    className=""
+    style={{
+      minHeight: '100vh',
+      backgroundImage: 'url(/bg.jpg)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundAttachment: 'fixed',
+    }}
+  >
     <Outlet />
     <TanStackRouterDevtools />
-  </>
+  </div>
 );
 
 export const Route = createRootRoute({ component: RootLayout });
