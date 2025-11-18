507d6b095cc184e6ab07a62fd2f502458affc9b8
OskarEriksen <omle.eriksen@gmail.com>
2025-11-18 13:15:01 +0100

    feat: signup method in auth service

--- backend/src/app/auth/auth.service.ts
+++ backend/src/app/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from '../user/user.service';
 import { User } from '../user/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import * as brycpt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -15,7 +17,7 @@ export class AuthService {
     hashedPassword: string,
   ): Promise<User | null> {
     const user = await this.usersService.findOne(email);
-    if (user && user.password === hashedPassword) {
+    if (user && user.hashedPassword === hashedPassword) {
       return user;
     }
     return null;
@@ -26,5 +28,25 @@ export class AuthService {
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
+  }
+
+  async signup(createUserDto: CreateUserDto) {
+    const user = await this.usersService.findOne(createUserDto.email);
+    if (user) {
+      throw new BadRequestException('User with this email already exists');
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await brycpt.hash(
+      createUserDto.password,
+      saltRounds,
+    );
+
+    const userWithHashedPassword = {
+      ...createUserDto,
+      hashedPassword: hashedPassword,
+    };
+
+    await this.usersService.create(userWithHashedPassword);
   }
 }
