7ffe1a82285532557d6a85cece41b9079287d85d
OskarEriksen <omle.eriksen@gmail.com>
2025-11-24 14:47:08 +0100

    feat: implement frontpage layout with card component

--- frontend-dashboard/src/app/routes/frontpage/index.tsx
+++ frontend-dashboard/src/app/routes/frontpage/index.tsx
@@ -1,9 +1,22 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import Logo from '../../../components/logo/Logo';
+import { Card, CardContent } from '@mui/material';
+import CreateReview from '../../../components/create-review/Create-review';
+import styles from './frontpage.module.css';
 
 export const Route = createFileRoute('/frontpage/')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/frontpage/"!</div>
+  return (
+    <div className={styles.card}>
+      <Logo />
+      <Card sx={{ bgcolor: 'primary.main', color: 'background.default' }}>
+        <CardContent>
+          <CreateReview />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
