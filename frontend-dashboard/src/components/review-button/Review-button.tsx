f47cffde9558898db13bff89805f1519d643fb19
OskarEriksen <omle.eriksen@gmail.com>
2025-11-25 12:58:30 +0100

    feat: add custom ReviewButton component with styles

---
+++ frontend-dashboard/src/components/review-button/Review-button.tsx
@@ -0,0 +1,37 @@
+import styles from './review-button.module.css';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
+import { Checkbox, FormControlLabel } from '@mui/material';
+
+type ReviewButtonProps = {
+  title: string;
+  done: boolean;
+  onClick?: () => void;
+};
+
+const ReviewButton = ({ title, done, onClick }: ReviewButtonProps) => {
+  return (
+    <div className={styles.reviewButton}>
+      <div className={styles.intro}>
+        <h4>{title}</h4>
+        {!done ? (
+          <CancelOutlinedIcon color="error" />
+        ) : (
+          <CheckCircleOutlineOutlinedIcon color="success" />
+        )}
+      </div>
+      <div className={styles.relevance}>
+        <FormControlLabel
+          control={<Checkbox />}
+          label="Ikke relevant at gennemgå"
+          labelPlacement="start"
+          className={styles.checkbox}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ReviewButton;
+
+// {isSubmitting ? 'Logger ind...' : 'Log ind'}
