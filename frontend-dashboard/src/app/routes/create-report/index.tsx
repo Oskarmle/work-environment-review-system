31dbf3fe89df25d74f21c51067483f2c46b802dc
OskarEriksen <omle.eriksen@gmail.com>
2025-11-25 12:59:10 +0100

    feat: add reviewButton to Create-report page and fix styling for cards

--- frontend-dashboard/src/app/routes/create-report/index.tsx
+++ frontend-dashboard/src/app/routes/create-report/index.tsx
@@ -3,6 +3,7 @@ import Logo from '../../../components/logo/Logo';
 import { Card, CardContent } from '@mui/material';
 import CreateReview from '../../../components/create-review/Create-review';
 import styles from './create-report.module.css';
+import ReviewButton from '../../../components/review-button/Review-button';
 
 export const Route = createFileRoute('/create-report/')({
   component: RouteComponent,
@@ -10,13 +11,34 @@ export const Route = createFileRoute('/create-report/')({
 
 function RouteComponent() {
   return (
-    <div className={styles.card}>
+    <>
       <Logo />
-      <Card sx={{ bgcolor: 'primary.main', color: 'background.default' }}>
-        <CardContent>
-          <CreateReview />
-        </CardContent>
-      </Card>
-    </div>
+      <div className={styles.content}>
+        <div className={styles['card-left']}>
+          <Card sx={{ bgcolor: 'primary.main', color: 'background.default' }}>
+            <CardContent>
+              <CreateReview />
+            </CardContent>
+          </Card>
+        </div>
+        <div className={styles['card-right']}>
+          <Card sx={{ bgcolor: 'primary.main', color: 'background.default' }}>
+            <CardContent>
+              <div className={styles['card-right-content']}>
+                {/* FIXME: Set done condition dependent on filled out content */}
+                <ReviewButton title="Bygning / inventar" done={false} />
+                <ReviewButton title="Kontorarbejdspladser" done={false} />
+                <ReviewButton title="Beredskab og førstehjælp" done={true} />
+                <ReviewButton title="Kemikalier" done={false} />
+                <ReviewButton
+                  title="Udeområde, Brandgårde, Øvelsesareal"
+                  done={false}
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </>
   );
 }
