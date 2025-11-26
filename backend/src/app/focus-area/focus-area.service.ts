0f639aceee0bdd52d1e34c7e0aea4a49558abac9
OskarEriksen <omle.eriksen@gmail.com>
2025-11-26 11:35:47 +0100

    feat: setup route and method for creating and fetching all focus areas

--- backend/src/app/focus-area/focus-area.service.ts
+++ backend/src/app/focus-area/focus-area.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FocusArea } from './entities/focus-area.entity';
+import { Repository } from 'typeorm';
+import { CreateFocusAreaDto } from './dto/focus-area.dto';
 
 @Injectable()
-export class FocusAreaService {}
+export class FocusAreaService {
+  constructor(
+    @InjectRepository(FocusArea)
+    private focusAreaRepository: Repository<FocusArea>,
+  ) {}
+
+  async create(createFocusAreaDto: CreateFocusAreaDto): Promise<FocusArea> {
+    const focusArea = this.focusAreaRepository.create(createFocusAreaDto);
+    return this.focusAreaRepository.save(focusArea);
+  }
+
+  async findAll(): Promise<FocusArea[]> {
+    return this.focusAreaRepository.find();
+  }
+}
