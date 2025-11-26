0115876037ab7b7c217789919b58585286279b57
OskarEriksen <omle.eriksen@gmail.com>
2025-11-26 17:30:21 +0100

    feat: refactor create method in SectionFieldService to use CreateSectionFieldDto

--- backend/src/app/section-field/section-field.service.ts
+++ backend/src/app/section-field/section-field.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SectionField } from './entities/section-field.entity';
 import { Repository } from 'typeorm';
+import { CreateSectionFieldDto } from './dto/section-field.dto';
 
 @Injectable()
 export class SectionFieldService {
@@ -10,12 +11,20 @@ export class SectionFieldService {
     private sectionFieldRepository: Repository<SectionField>,
   ) {}
 
-  async create(sectionFieldData: Partial<SectionField>): Promise<SectionField> {
-    const sectionField = this.sectionFieldRepository.create(sectionFieldData);
+  async create(
+    createSectionFieldDto: CreateSectionFieldDto,
+  ): Promise<SectionField> {
+    const { sectionId, ...sectionFieldData } = createSectionFieldDto;
+
+    const sectionField = this.sectionFieldRepository.create({
+      ...sectionFieldData,
+      section: { id: sectionId },
+    });
+
     return this.sectionFieldRepository.save(sectionField);
   }
 
   async findAll(): Promise<SectionField[]> {
-    return this.sectionFieldRepository.find();
+    return this.sectionFieldRepository.find({ relations: ['section'] });
   }
 }
