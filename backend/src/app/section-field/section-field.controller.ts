9fef8d6169d4681fe7365eb0029e384169957277
OskarEriksen <omle.eriksen@gmail.com>
2025-11-26 16:01:42 +0100

    feat: setup route and method for fetching and posting section-fields

--- backend/src/app/section-field/section-field.controller.ts
+++ backend/src/app/section-field/section-field.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { SectionFieldService } from './section-field.service';
+import { CreateSectionFieldDto } from './dto/section-field.dto';
+import { SectionField } from './entities/section-field.entity';
 
 @Controller('section-field')
-export class SectionFieldController {}
+export class SectionFieldController {
+  constructor(private sectionFieldService: SectionFieldService) {}
+
+  @Post()
+  async create(
+    @Body() createSectionFieldDto: CreateSectionFieldDto,
+  ): Promise<SectionField> {
+    return this.sectionFieldService.create(createSectionFieldDto);
+  }
+
+  @Get()
+  async findAll(): Promise<SectionField[]> {
+    return this.sectionFieldService.findAll();
+  }
+}
--- backend/src/app/section-field/section-field.service.ts
+++ backend/src/app/section-field/section-field.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SectionField } from './entities/section-field.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class SectionFieldService {}
+export class SectionFieldService {
+  constructor(
+    @InjectRepository(SectionField)
+    private sectionFieldRepository: Repository<SectionField>,
+  ) {}
+
+  async create(sectionFieldData: Partial<SectionField>): Promise<SectionField> {
+    const sectionField = this.sectionFieldRepository.create(sectionFieldData);
+    return this.sectionFieldRepository.save(sectionField);
+  }
+
+  async findAll(): Promise<SectionField[]> {
+    return this.sectionFieldRepository.find();
+  }
+}
