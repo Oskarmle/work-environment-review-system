529627682ee2bc60319526d0a18dfaac3b6008f7
OskarEriksen <omle.eriksen@gmail.com>
2025-11-27 13:10:29 +0100

    feat: create dto for station and report

---
+++ backend/src/app/report/dto/create-report.dto.ts
@@ -0,0 +1,19 @@
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateReportDto {
+  @IsOptional()
+  @IsString()
+  comment: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  IsCompleted: boolean;
+
+  @IsNotEmpty()
+  @IsString()
+  focusAreaId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  stationId: string;
+}
