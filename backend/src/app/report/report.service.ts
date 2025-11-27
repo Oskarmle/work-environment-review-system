f8faf3c5a22244a00ac6f5653dab31bb4d2e0e57
OskarEriksen <omle.eriksen@gmail.com>
2025-11-27 13:10:14 +0100

    feat: report method and routes for getall, getone and create

---
+++ backend/src/app/report/report.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './entities/report.entity';
+import { FindOneOptions, Repository } from 'typeorm';
+import { CreateReportDto } from './dto/create-report.dto';
+
+@Injectable()
+export class ReportService {
+  constructor(
+    @InjectRepository(Report)
+    private reportRepository: Repository<Report>,
+  ) {}
+
+  async create(createReportDto: CreateReportDto): Promise<Report> {
+    const report = this.reportRepository.create(createReportDto);
+    return this.reportRepository.save(report);
+  }
+
+  findOne(options: FindOneOptions<Report>): Promise<Report | null> {
+    return this.reportRepository.findOne(options);
+  }
+
+  findAll(): Promise<Report[]> {
+    return this.reportRepository.find();
+  }
+}
