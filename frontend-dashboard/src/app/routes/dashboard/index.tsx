03398e47554c1ce727f52036d01df1d519e1179d
OskarEriksen <omle.eriksen@gmail.com>
2025-12-04 14:40:02 +0100

    feat: setup pages for route and styling

---
+++ frontend-dashboard/src/app/routes/dashboard/index.tsx
@@ -0,0 +1,18 @@
+import { createFileRoute } from '@tanstack/react-router';
+import styles from './dashboard.module.css';
+import Logo from '../../../components/logo/Logo';
+
+export const Route = createFileRoute('/dashboard/')({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div className={styles.dashboard}>
+      <Logo />
+      <div>
+        
+      </div>
+    </div>
+  );
+}
