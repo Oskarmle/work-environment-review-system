6ab9ed3d851eba0c0a77ebc480e68eeaa663f895
OskarEriksen <omle.eriksen@gmail.com>
2025-12-05 12:29:03 +0100

    feat: implement delete functionality for initial checks on dashboard

--- frontend-dashboard/src/app/routes/dashboard/index.tsx
+++ frontend-dashboard/src/app/routes/dashboard/index.tsx
@@ -3,14 +3,17 @@ import styles from './dashboard.module.css';
 import Logo from '../../../components/logo/Logo';
 import { Card, CardContent } from '@mui/material';
 import InitialCheckDashboardList from '../../../components/initial-check-dashboard-list/Initial-check-dashboard-list';
+import { useDeleteInitialCheck } from '../../../hooks/useRemoveInitialCheck';
 
 export const Route = createFileRoute('/dashboard/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const deleteInitialCheck = useDeleteInitialCheck();
+
   const handleDelete = (id: string) => {
-    console.log('Delete item', id);
+    deleteInitialCheck.mutate(id);
   };
 
   return (
