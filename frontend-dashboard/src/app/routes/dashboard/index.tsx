bd473551198eb2f1545133187fb9c86d554e4e09
OskarEriksen <omle.eriksen@gmail.com>
2025-12-05 12:01:34 +0100

    feat: add initial check list component to dashboard

--- frontend-dashboard/src/app/routes/dashboard/index.tsx
+++ frontend-dashboard/src/app/routes/dashboard/index.tsx
@@ -1,16 +1,33 @@
 import { createFileRoute } from '@tanstack/react-router';
 import styles from './dashboard.module.css';
 import Logo from '../../../components/logo/Logo';
+import { Card, CardContent } from '@mui/material';
+import InitialCheckDashboardList from '../../../components/initial-check-dashboard-list/Initial-check-dashboard-list';
 
 export const Route = createFileRoute('/dashboard/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const handleDelete = (id: string) => {
+    console.log('Delete item', id);
+  };
+
   return (
     <div>
       <Logo />
-      <div className={styles.dashboard}></div>
+      <div className={styles.dashboard}>
+        <Card sx={{ bgcolor: 'primary.main', color: 'background.default' }}>
+          <CardContent className={styles['card-content']}>
+            <div>
+              <h3>Egenindsats punkter</h3>
+            </div>
+            <div>
+              <InitialCheckDashboardList handleDelete={handleDelete} />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
