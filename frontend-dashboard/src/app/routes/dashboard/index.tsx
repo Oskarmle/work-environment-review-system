bd50a6d4de0e0cf732d4c2696ce339300e64126c
Oskar Eriksen <121121527+Oskarmle@users.noreply.github.com>
2025-12-05 13:29:39 +0100

    Make dynamic initial checks (#9)
    * feat: initial check dashboard list component
    
    * feat: add initial check list component to dashboard
    
    * feat: add delete functionality to initial check controller and service
    
    * feat: implement delete functionality for initial checks on dashboard
    
    * feat: create hook to create initial checks
    
    * feat: make component to contain initial check modal
    
    * feat: add header and initial check modal to dashboard component
    
    * fix: clean up imports

--- frontend-dashboard/src/app/routes/dashboard/index.tsx
+++ frontend-dashboard/src/app/routes/dashboard/index.tsx
@@ -1,16 +1,52 @@
 import { createFileRoute } from '@tanstack/react-router';
 import styles from './dashboard.module.css';
 import Logo from '../../../components/logo/Logo';
+import { Card, CardContent, Fab } from '@mui/material';
+import InitialCheckDashboardList from '../../../components/initial-check-dashboard-list/Initial-check-dashboard-list';
+import { useDeleteInitialCheck } from '../../../hooks/useRemoveInitialCheck';
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
+import CreateInitialCheckModal from '../../../components/create-initial-check-modal/Create-initial-check-modal';
 
 export const Route = createFileRoute('/dashboard/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const deleteInitialCheck = useDeleteInitialCheck();
+
+  const handleDelete = (id: string) => {
+    deleteInitialCheck.mutate(id);
+  };
+
   return (
     <div>
       <Logo />
-      <div className={styles.dashboard}></div>
+      <div className={styles.dashboard}>
+        <Card sx={{ bgcolor: 'primary.main', color: 'background.default' }}>
+          <CardContent className={styles['card-content']}>
+            <div className={styles.header}>
+              <h3>Egenindsats punkter</h3>
+              <Fab
+                size="small"
+                color="secondary"
+                aria-label="add"
+                onClick={handleOpen}
+              >
+                <AddIcon />
+              </Fab>
+            </div>
+            <div>
+              <InitialCheckDashboardList handleDelete={handleDelete} />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+      <CreateInitialCheckModal open={open} handleClose={handleClose} />
     </div>
   );
 }
