91ffe1d1ea8406af1acf062eaedf1f7dd7c29022
OskarEriksen <omle.eriksen@gmail.com>
2025-12-08 13:03:14 +0100

    feat: setup navigation to frontPage

--- frontend-dashboard/src/components/logo/Logo.tsx
+++ frontend-dashboard/src/components/logo/Logo.tsx
@@ -1,14 +1,36 @@
+import { useNavigate, useRouter } from '@tanstack/react-router';
 import styles from './logo.module.css';
+import { Button } from '@mui/material';
 
 const Logo = () => {
+  const router = useRouter();
+  const currentPath = router.state.location.pathname;
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate({ to: '/frontpage' });
+  };
+
   return (
-    <div className={styles.logo}>
-      <img
-        src="images/HBR-Logo-bredt-hvid.png"
-        alt="Logo"
-        className={styles.logoImage}
-      />
-      <h1>Arbejdsmiljørundering</h1>
+    <div className={styles.logoContainer}>
+      <div className={styles.logo}>
+        <img
+          src="images/HBR-Logo-bredt-hvid.png"
+          alt="Logo"
+          className={styles.logoImage}
+        />
+        <h1>Arbejdsmiljørundering</h1>
+      </div>
+      {currentPath !== '/frontpage' && (
+        <Button
+          className={styles.button}
+          color="secondary"
+          onClick={handleNavigation}
+          variant="contained"
+        >
+          Forside
+        </Button>
+      )}
     </div>
   );
 };
