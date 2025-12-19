7d328d42a1bbc30d97c3f31e910ca4c1ee27e21e
OskarEriksen <omle.eriksen@gmail.com>
2025-12-19 11:19:45 +0100

    feat: add pdf module to app module

--- backend/src/app/pdf/pdf.module.ts
+++ backend/src/app/pdf/pdf.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PdfController } from './pdf.controller';
+import { PdfService } from './pdf.service';
 
-@Module({})
+@Module({
+  controllers: [PdfController],
+  providers: [PdfService],
+})
 export class PdfModule {}
