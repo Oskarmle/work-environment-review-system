8efc9d94d44a8bc7bb51f2469ca6919730cf1c54
OskarEriksen <omle.eriksen@gmail.com>
2026-01-13 12:13:20 +0100

    test: enhance KeycloakController tests with mock KeycloakService

--- backend/src/app/keycloak/keycloak.controller.spec.ts
+++ backend/src/app/keycloak/keycloak.controller.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { KeycloakController } from './keycloak.controller';
+import { KeycloakService } from './keycloak.service';
 
 describe('KeycloakController', () => {
   let controller: KeycloakController;
 
+  const mockKeycloakService = {
+    getUsers: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [KeycloakController],
+      providers: [
+        {
+          provide: KeycloakService,
+          useValue: mockKeycloakService,
+        },
+      ],
     }).compile();
 
     controller = module.get<KeycloakController>(KeycloakController);
