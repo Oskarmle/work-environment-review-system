6d935435ae02b4a4d935f2ae63145cfeac724fa6
OskarEriksen <omle.eriksen@gmail.com>
2026-01-13 12:13:05 +0100

    test: enhance InitialCheckController tests with mock InitialCheckService

--- backend/src/app/initial-check/initial-check.controller.spec.ts
+++ backend/src/app/initial-check/initial-check.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InitialCheckController } from './initial-check.controller';
+import { InitialCheckService } from './initial-check.service';
 
 describe('InitialCheckController', () => {
   let controller: InitialCheckController;
 
+  const mockInitialCheckService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [InitialCheckController],
+      providers: [
+        {
+          provide: InitialCheckService,
+          useValue: mockInitialCheckService,
+        },
+      ],
     }).compile();
 
     controller = module.get<InitialCheckController>(InitialCheckController);
