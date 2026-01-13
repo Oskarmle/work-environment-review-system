668332825068e4efeb76813ea2058973da6c5467
OskarEriksen <omle.eriksen@gmail.com>
2026-01-13 12:14:03 +0100

    test: enhance SectionFieldResponseController tests with mock SectionFieldResponseService

--- backend/src/app/section-field-response/section-field-response.controller.spec.ts
+++ backend/src/app/section-field-response/section-field-response.controller.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SectionFieldResponseController } from './section-field-response.controller';
+import { SectionFieldResponseService } from './section-field-response.service';
 
 describe('SectionFieldResponseController', () => {
   let controller: SectionFieldResponseController;
 
+  const mockSectionFieldResponseService = {
+    create: jest.fn(),
+    findAllForAReport: jest.fn(),
+    createMany: jest.fn(),
+    updateSectionFieldResponses: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SectionFieldResponseController],
+      providers: [
+        {
+          provide: SectionFieldResponseService,
+          useValue: mockSectionFieldResponseService,
+        },
+      ],
     }).compile();
 
     controller = module.get<SectionFieldResponseController>(
